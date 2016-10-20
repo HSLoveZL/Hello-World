2f7c1a314d316acaebc0c8a2fbf160a1ca1121f0
HSLoveZL <hjmrezl@outlook.com>
2016-10-20 20:18:47 +0800

    Update 2016-10-20StudyPython.py
    Exercise in the use of args

--- 2016-10-20StudyPython.py
+++ 2016-10-20StudyPython.py
@@ -1,1 +1,21 @@
-def story(**names)
+def story(**names):
+  return 'Once upon a time, there was a ' \
+         '%(jobs)s called %(name)s.' % names
+
+  
+def power(x, y, *others):
+  if others:
+    print 'Received redundant parameters:', others
+  return pow(x, y)
+
+
+def interval(start, stop=None, step=1):
+  'Imitates range() for step > 0'
+  if stop is None:
+    start, stop = 0; start
+  result = []
+  i = start
+  while i < stop:
+    result.append(i)
+    i += step
+  return result
